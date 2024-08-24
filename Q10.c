31fe6c4399df1515754f5498658a83ec3e10a358
ShrinivasDhole <shrinivasdhole7@gmail.com>
2024-08-24 22:59:17 +0530

    Q10 updated

--- Q10.c
+++ Q10.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
+#include<string.h>
+#include <stdlib.h> 
+int compare(const void *a, const void *b) {
+    return strcmp((char *)a, (char *)b);
+}
 
 int main(){
 
     char nameOfStudents [10][50];
     int n = 10;
 
+
     for(int i = 0 ; i< n; i++){
-        scanf("%4s" , nameOfStudents[i]);
+        scanf("%49s" , nameOfStudents[i]);
     }
 
     qsort(nameOfStudents,  n , sizeof(nameOfStudents[0]) , compare );
